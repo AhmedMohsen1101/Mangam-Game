531acbea1b2579a2c0c6129643e49bbfb96123ed
Ahmed Mohsen <50053704+AhmedMohsen1101@users.noreply.github.com>
2021-08-15 17:39:09 +0200

    setup environment

---
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum Type
+{
+    Player,
+    Enemy,
+}
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float turnSpeed = 15;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+    private void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void Move(Vector3 movement)
+    {
+        agent.Move(movement.normalized * Time.fixedDeltaTime * moveSpeed);
+        animator.SetFloat("Movement", movement.magnitude);
+
+        if(movement.magnitude > 0.01f)
+        {
+            Quaternion smoothRotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(movement), Time.fixedDeltaTime * turnSpeed);
+            transform.rotation = smoothRotation;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private VariableJoystick joystick;
+    private PlayerController playerController;
+
+    private void OnEnable()
+    {
+        if (playerController == null)
+        {
+            playerController = GetComponent<PlayerController>();
+        }
+
+        if(joystick == null)
+        {
+            joystick = GameObject.FindObjectOfType<VariableJoystick>();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerController == null || joystick == null)
+            return;
+
+        playerController.Move(new Vector3(joystick.Horizontal, 0, joystick.Vertical));
+    }
+
+}
